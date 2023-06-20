184717d96993f183baa8c1de246bd60ffdc91570
MarianaRosa101 <marianarosa100@gmail.com>
2023-06-20 10:29:49 -0300

    final commit

--- src/app/empresa-cadastro/empresa-cadastro.component.ts
+++ src/app/empresa-cadastro/empresa-cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Empresa } from '../empresa.model';
+import { EmpresaService } from '../empresa.service';
 
 @Component({
   selector: 'app-empresa-cadastro',
@@ -7,34 +9,47 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./empresa-cadastro.component.css']
 })
 export class EmpresaCadastroComponent {
-    empresaForm: FormGroup;
-    empresas: any[] = [];
-  
-    constructor(private formBuilder: FormBuilder) {
-      this.empresaForm = this.formBuilder.group({
-        nome: ['', Validators.required],
-        cnpj: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        telefone: ['', Validators.required]
+  empresaForm: FormGroup;
+  empresas: Empresa[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private empresaService: EmpresaService
+  ) {
+    this.empresaForm = this.formBuilder.group({
+      nome: ['', Validators.required],
+      cnpj: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', Validators.required]
+    });
+  }
+
+  cadastrar() {
+    if (this.empresaForm.valid) {
+      const novaEmpresa: Empresa = {
+        nome: this.empresaForm.value.nome,
+        cnpj: this.empresaForm.value.cnpj,
+        email: this.empresaForm.value.email,
+        telefone: this.empresaForm.value.telefone,
+        id: 0
+      };
+
+      this.empresaService.cadastrarEmpresa(novaEmpresa).subscribe(() => {
+        this.empresaForm.reset();
+        this.atualizarListaEmpresas();
       });
     }
-  
-    cadastrar() {
-      if (this.empresaForm.valid) {
-        const novaEmpresa = {
-          id: this.empresas.length + 1,
-          nome: this.empresaForm.value.nome,
-          cnpj: this.empresaForm.value.cnpj,
-          email: this.empresaForm.value.email,
-          telefone: this.empresaForm.value.telefone
-        };
-        this.empresas.push(novaEmpresa);
-        this.empresaForm.reset();
-      }
-    }
-  
-    remover(id: number) {
-      this.empresas = this.empresas.filter(empresa => empresa.id !== id);
-    }
   }
-  
+
+  remover(id: number) {
+    this.empresaService.removerEmpresa(id).subscribe(() => {
+      this.empresas = this.empresas.filter(empresa => empresa.id !== id);
+    });
+  }
+
+  private atualizarListaEmpresas() {
+    this.empresaService.getEmpresas().subscribe(empresas => {
+      this.empresas = empresas;
+    });
+  }
+}
